1bcd7029a571400f95bf6325ea42c53399b3a118
CLF1992108 <791260656@qq.com>
2020-11-17 06:39:38 +0800

    VUE学习案例

---
+++ kai_ke_ba/date05/app1.js
@@ -0,0 +1,10 @@
+let App1 = {
+    template:`<div>我是组件</div>`,
+    data(){
+        return {}
+    }
+}
+export var sum = 1;
+var sum1 = 2;
+export {sum1}
+export default App1;
---
+++ kai_ke_ba/date05/main.js
@@ -0,0 +1,4 @@
+import * as App from './app.js';
+import * as App1 from './app1.js';
+console.log(App.sum);
+console.log(App1.sum1);
